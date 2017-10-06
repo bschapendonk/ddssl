b6c19b095ce5955ac2e3ccddc9eed93ce47b6fdd
Bart Schapendonk <bart.schapendonk@topicus.nl>
2017-10-06 16:40:23 +0200

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 #include <Arduino.h>
+#include <WiFiClientSecure.h>
+
+#include <AzureIoTHub.h>
+#include <AzureIoTUtility.h>
+#include <AzureIoTProtocol_HTTP.h>
+
+const char* ssid     = "your-ssid";
+const char* password = "your-password";
 
 void setup() {
-    // put your setup code here, to run once:
+    Serial.begin(115200);
+    delay(10);
+
+    Serial.println();
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
