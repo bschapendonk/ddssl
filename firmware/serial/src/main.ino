a8dc888d773e1e43a882a6a19a1055cd59e16cdd
Bart Schapendonk <bart.schapendonk@topicus.nl>
2017-12-08 15:07:05 +0100

    update

--- firmware/serial/src/main.ino
+++ firmware/serial/src/main.ino
@@ -1,9 +1,34 @@
 #include <Arduino.h>
 
-void setup() {
-    // put your setup code here, to run once:
+void setup()
+{
+    ledcSetup(1, 10000, 8);
+    ledcSetup(2, 10000, 8);
+    ledcSetup(3, 10000, 8);
+
+    ledcAttachPin(25, 1);
+    ledcAttachPin(26, 2);
+    ledcAttachPin(27, 3);
+
+    int ledArray[3] = {1, 2, 3};
+    for (int i = 0; i < 3; i++)
+    {
+        ledcWrite(ledArray[i], 255);
+        delay(1000);
+        ledcWrite(ledArray[i], 0);
+    }
+
+    Serial.begin(115200);
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+    byte buffer[3];
+    if (Serial.available() > 0)
+    {
+        Serial.readBytes(buffer, 3);
+        ledcWrite(1, buffer[0]);
+        ledcWrite(2, buffer[1]);
+        ledcWrite(3, buffer[2]);
+    }
 }
